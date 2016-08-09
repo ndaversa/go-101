f7211ab6a94ed00c25b81ad800d3bfd48fafae47
Nino D'Aversa <nino@ndaversa.com>
2016-08-09 14:13:59 -0400

    Cleanup styling

--- exercises/ex1/main.go
+++ exercises/ex1/main.go
@@ -1,4 +1,3 @@
-// Declare a struct type to contain information about a person.  Include a name and age.  Declare a function that pretty-prints values of your type.  Create a slice of people.  In `main` call your pretty-printer for each person.
 package main
 
 import "fmt"
@@ -8,23 +7,17 @@ type Person struct {
 	Age  int
 }
 
-func PrettyPerson(p Person) string {
+func Pretty(p Person) string {
 	return fmt.Sprintf("%s is %d years old", p.Name, p.Age)
 }
 
 func main() {
 	ppl := []Person{
-		Person{
-			Name: "Nino",
-			Age:  33,
-		},
-		Person{
-			Name: "Jonathan",
-			Age:  38,
-		},
+		Person{Name: "Nino", Age: 33},
+		Person{Name: "Jonathan", Age: 38},
 	}
 
-	for _, v := range ppl {
-		fmt.Println(PrettyPerson(v))
+	for _, p := range ppl {
+		fmt.Println(Pretty(p))
 	}
 }
